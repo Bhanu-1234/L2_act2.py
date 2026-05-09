37106260afcfdfd58b5ee9b1d710683d41cedee3
Bhanu-1234 <59434447+Bhanu-1234@users.noreply.github.com>
2026-05-09 11:06:16 -0700

    Add files via upload

---
+++ L2_act2.py
@@ -0,0 +1,14 @@
+#Write a program to create different variables to store various values.
+#Creat different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name:")
+
+print("\nHello", name, "\nwelcome to codingal")
